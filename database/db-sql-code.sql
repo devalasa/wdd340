5873ccba2e661a0162e694e3b64696e285497198
Richard <ogb22001@byui.edu>
2025-04-03 22:26:56 +0100

    week 3 half done

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -50,7 +50,18 @@ CREATE TABLE IF NOT EXISTS public.account
 -- Data for table 'classification'
 INSERT INTO public.classification (classification_name)
 VALUES ('Custom'),
-	('Sport')
-	('SUV')
-	('Truck')
+	('Sport'),
+	('SUV'),
+	('Truck'),
 	('Sedan');
+
+-- Query 4
+UPDATE inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- Query 6
+UPDATE inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -26,11 +28,11 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
 
 
 
@@ -47,3 +49,4 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+
