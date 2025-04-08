a77db245df7967f6aaa8b898fc7107e5d62a46fd
Richard <ogb22001@byui.edu>
2025-04-08 15:06:19 +0100

    week4

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const static = require("./routes/static")
 const inventoryRoute = require("./routes/inventoryRoute")
 const baseController = require("./controllers/baseController")
 const utilities = require("./utilities/")
+const session = require("express-session")
+const pool = require('./database/')
 
 /* ***********************
  * View Engine and Templates
@@ -50,7 +52,9 @@ app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
 
-
+/* ***********************
+ * Middleware
+ * ************************/
 // Global error handler (500 errors)
 app.use(async (err, req, res, next) => {
   console.error(err.stack);
@@ -63,5 +67,20 @@ app.use(async (err, req, res, next) => {
   });
 });
 
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+  }))
 
-
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
